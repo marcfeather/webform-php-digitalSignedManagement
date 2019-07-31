05fc427b04ec1c7fbefadbc7ec860e451cce7cf8
warayut <warac575@iptelephony.ais.co.th>
2019-07-31 17:11:23 +0700

    Coding 5

--- gallery.php
+++ gallery.php
@@ -1,67 +1,56 @@
 <?php
 
-$folder_name = 'content/';
-
 if(isset($_POST["name"]))
 {
     $filename = $folder_name.$_POST["name"];
     unlink($filename);
 
     include("mysqli_connect.php"); 
-    $value = $_POST["value"];
-    $sql = "DELETE FROM contents WHERE content_name = '".$value."'";
+    $id = $_POST["id"];
+    $sql = "DELETE FROM contents WHERE content_id = '".$id."'";
     $conn->query($sql);
     $conn->close();
 }
 
-//$result = array();
-
-//$files = scandir('content');
-
 $output = '<div class="row">';
 
-$files = array();
-$dir = opendir('content'); // open the cwd..also do an err check.
+include("mysqli_connect.php");
 
-while(false != ($file = readdir($dir))) {
-        if(($file != ".") and ($file != "..") and ($file != "index.php")) {
-                $files[] = $file; // put in array.
-        }   
-}
+$sql = "SELECT content_id, content_order, content_name, content_type, content_url FROM contents ORDER BY content_order";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $id = $row['content_id'];
+        $order = $row['content_order'];
+        $filename = $row['content_name'];
+        $filenameExt = $row['content_name'] . '.' . $row['content_type'];
+        $filepath = $row['content_url'] . $row['content_name'] . "." . $row['content_type'];
 
-//natsort($files); // sort.
-
-if(false !== $files)
-{
- foreach($files as $file)
- {
-  if('.' !=  $file && '..' != $file)
-  {
-    $name = pathinfo($folder_name.$file, PATHINFO_FILENAME);
-   $output .= '
-   <div class="col-md-55">
-    <div class="thumbnail">
-        <div class="image view view-first">
-            <img style="width: 100%; display: block;" src="'.$folder_name.$file.'" alt="image" />
-        </div>
-        <div class="caption">
-            <div class="row">
-                <div class="col-xs-6 col-sm-6 col-md-6" align="left">
-                    <p>'.$name.'</p>
+        $output .= '
+        <div class="col-md-55">
+            <div class="thumbnail">
+                <div class="image view view-first">
+                    <img style="width: 100%; display: block;" src="'.$filepath.'" alt="image" />
                 </div>
-                <div class="col-xs-6 col-sm-6 col-md-6" align="right">
-                    <button type="button" class="btn btn-danger btn-xs remove_image" id="'.$file.'" value="'.$name.'">
-                        <i class="fa fa-close"></i> ลบ
-                    </button>
+                <div class="caption">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6" align="left">
+                            <p>'.$filenameExt.'</p>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6" align="right">
+                            <button type="button" class="btn btn-danger btn-xs remove_image" id="'.$id.'" value="'.$filename.'">
+                                <i class="fa fa-close"></i> ลบ
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-   </div>
-   ';
-  }
- }
+        ';
+    }
 }
+
 $output .= '</div>';
 echo $output;
 
--- script/index.js
+++ script/index.js
@@ -80,12 +80,13 @@ $(document).ready(function(){
     };
    
     $(document).on('click', '.remove_image', function(){
-        var name = $(this).attr('id');
-        var value = $(this).attr('value');
+        var id = $(this).attr('id');
+        var name = $(this).attr('value');
         $.ajax({
             url:"gallery.php",
             method:"POST",
-            data:{name:name,value:value},
+            //data:{name:name,value:value},
+            data:{id:id,name:name},
             success:function(data)
             {
                 list_image();
